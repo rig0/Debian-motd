2f1027462d0b9f5dadd2cb7a2490735571800497
Rambo <ramborigs90@gmail.com>
2024-11-17 15:10:55 +0000

    basic first iteration

---
+++ motd-setup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Install prerequisites
+apt install lolcat linuxlogo toilet figlet cowsay fortune
+
+# Backup up original motd
+mv /etc/motd /etc/motd.bak
+
+# Comment out original linux os and kernel info
+sed -i -e 's/uname -snrvm/#uname -snrvm/g' /etc/update-motd.d/10-uname
+
+# Create custom message
+touch /etc/update-motd.d/99-custom-motd
+echo "#!/bin/bash" >> /etc/update-motd.d/99-custom-motd
+echo "{" >> /etc/update-motd.d/99-custom-motd
+echo "  /usr/games/fortune | /usr/games/cowsay -f tux" >> /etc/update-motd.d/99-custom-motd
+echo "  toilet -f ivrit \"$HOSTNAME\"" >> /etc/update-motd.d/99-custom-motd
+echo "  linuxlogo -a -g -u -d -s -k -F \"Debian $(cat /etc/debian_version) Bookworm \n#O Kernel #V \n#M #T #R RAM \n#U\"" >> /etc/update-motd.d/99-custom-motd
+echo "} | /usr/games/lolcat -p 13 --force" >> /etc/update-motd.d/99-custom-motd
