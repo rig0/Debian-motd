ba4af66da3bcdfea26cb1d8cdfee0e7066be06be
Rambo <ramborigs90@gmail.com>
2024-11-17 15:47:20 +0000

    make 99-custom-motd executable

--- motd-setup.sh
+++ motd-setup.sh
@@ -18,3 +18,4 @@ echo "  /usr/games/fortune | /usr/games/cowsay -f tux" >> /etc/update-motd.d/99-
 echo "  toilet -f ivrit \"$HOSTNAME\"" >> /etc/update-motd.d/99-custom-motd
 echo "  linuxlogo -a -g -u -d -s -k -F \"Debian $(cat /etc/debian_version) Bookworm \n#O Kernel #V \n#M #T #R RAM \n#U\"" >> /etc/update-motd.d/99-custom-motd
 echo "} | /usr/games/lolcat -p 13 --force" >> /etc/update-motd.d/99-custom-motd
+chmod +x /etc/update-motd.d/99-custom-motd
